c68b209a2c901d83c492ebbb22a228b3587c8783
SHREYAS <shreyask72201@gmail.com>
2025-11-03 14:05:07 +0530

    Update database password in db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Krish@123",
+  password: "Shreyas@123",
   database: "findit",
 });
 
